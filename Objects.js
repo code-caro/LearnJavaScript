7b03213039fdc499b4258e5ebeab7b700aca15f4
Caro <81607208+code-caro@users.noreply.github.com>
2022-01-28 21:23:38 +0100

    Update Objects.js
    add function using objects for lookup

--- Objects.js
+++ Objects.js
@@ -5,3 +5,26 @@ const myDog = {
   "tails": 1,
   "friends": ["Lucy", "Edgar"]
 };
+
+myDog.name = "Willi"; /* or myDog["name"] = "Willi"; */
+myDog.bark = "woof";  /* or myDog["bark"] = "woof"; */
+delete myDog["tails"]; /* or delete myDog.tails; */
+
+
+/*using objects for lookups*/
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  }
+  result = lookup[val];
+  return result;
+}
+
+phoneticLookup("charlie");
