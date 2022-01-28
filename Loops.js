c9571dbfed6736c441c2b34efa48c11efaa71896
Caro <81607208+code-caro@users.noreply.github.com>
2022-01-28 22:52:48 +0100

    Update Loops.js
    multiply all

--- Loops.js
+++ Loops.js
@@ -40,3 +40,18 @@ var total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i]; 
 }
+
+/*multiply all*/
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
