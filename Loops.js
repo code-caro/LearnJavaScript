9d62162ddd7b905b13aa1085d87dd46e60870c64
Caro <81607208+code-caro@users.noreply.github.com>
2022-01-28 22:29:15 +0100

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,18 @@
+/*while*/
+// Setup
+var myArray1 = [];
+var i = 5;
+
+while(i >= 0) {
+  myArray1.push(i);
+  i--;
+}
+
+
+/*for*/
+// Setup
+const myArray2 = [];
+
+for (var i = 1; i <= 5; i++){
+  myArray2.push(i);
+}
