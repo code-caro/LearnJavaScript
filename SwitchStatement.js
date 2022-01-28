48429d9ff682390a4b799388a57926c2e8b3237c
Caro <81607208+code-caro@users.noreply.github.com>
2022-01-28 20:04:26 +0100

    Update SwitchStatement.js
    add second function with a default option in switch statements

--- SwitchStatement.js
+++ SwitchStatement.js
@@ -1,8 +1,6 @@
 /*Selecting from Many Options with Switch Statements*/
-
 function caseInSwitch(val) {
   let answer = "";
-  // Only change code below this line
   switch(val) {
   case 1:
     answer = "alpha";
@@ -17,11 +15,29 @@ function caseInSwitch(val) {
     answer = "delta";
     break;
 }
-
-
-
-  // Only change code above this line
   return answer;
 }
 
 caseInSwitch(1);
+
+
+/*Adding a default option in switch statements */
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+  }
+  return answer;
+}
+
+switchOfStuff(1);
